cea0e24d5fd39499ff28cced91b9b3ec547d99f2
AlexFame <132550321+AlexFame@users.noreply.github.com>
2025-12-21 04:20:03 +0100

    chore: add error boundary to debug crash

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,26 @@ import { CartProvider } from './context/CartContext';
 import CartPage from './pages/CartPage';
 import { useTelegram } from './hooks/useTelegram';
 
+import ErrorBoundary from './components/ErrorBoundary';
+
 function App() {
   useTelegram(); // Initialize UA initialization
 
   return (
-    <AudioProvider>
-      <CartProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path="cart" element={<CartPage />} />
-            </Route>
-          </Routes>
-        </Router>
-      </CartProvider>
-    </AudioProvider>
+    <ErrorBoundary>
+      <AudioProvider>
+        <CartProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<HomePage />} />
+                <Route path="cart" element={<CartPage />} />
+              </Route>
+            </Routes>
+          </Router>
+        </CartProvider>
+      </AudioProvider>
+    </ErrorBoundary>
   );
 }
 
